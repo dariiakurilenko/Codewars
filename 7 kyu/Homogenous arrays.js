1b1d69f3e3ab16c5678e82fc351bb870f517fab0
Daria <113102018+dariiakurilenko@users.noreply.github.com>
2024-02-09 07:32:14 +0300

    Update Homogenous arrays.js

--- 7 kyu/Homogenous arrays.js
+++ 7 kyu/Homogenous arrays.js
@@ -1,5 +1,8 @@
 //Given a two-dimensional array, return a new array which carries over only those arrays from the original, which were not empty and whose items are all of the same type (i.e. homogenous). For simplicity, the arrays inside the array will only contain characters and integers.
 
 function filterHomogenous(arrays) {
-   return arrays.filter(b => b.length > 0 && b.every(e => typeof e == typeof b[0]))
+  return arrays.filter(arr => {
+    if (arr.length && arr.every(elem => typeof elem === 'number'))  return arr 
+    if (arr.length && arr.every(elem => typeof elem === 'string'))  return arr
+  })
 }
