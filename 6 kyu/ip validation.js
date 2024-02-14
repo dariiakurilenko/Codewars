ce2c1665cdacae51a26684afc7cba554e3551d4c
Daria <113102018+dariiakurilenko@users.noreply.github.com>
2024-02-14 12:16:05 +0300

    Update ip validation.js

--- 6 kyu/ip validation.js
+++ 6 kyu/ip validation.js
@@ -1,14 +1,32 @@
 function isValidIP(str) {
-    let normalIp = str.split(',').join(',').split('.').filter(a => a != '' && a[1]!='e')
-    let count = 0
-    if (normalIp.length == 4) {
-        for (let i = 0; i < normalIp.length; i++) {
-            if (normalIp[i] > 0 && normalIp[i] <= 255 && normalIp[i][0] !=='0'){
-                count +=1
-            }
+    const octets = str.split('.');
+    
+    // Проверяем количество октетов
+    if (octets.length !== 4) {
+        return false;
+    }
+
+    // Проверяем каждый октет
+    for (let i = 0; i < octets.length; i++) {
+        const octet = octets[i];
+        
+        if (octet.length === 0 || octet.length > 3) {
+            return false;
+        }
+
+        if (octet[0] === '0' && octet.length > 1) {
+            return false;
+        }
+
+        if (!/^\d+$/.test(octet)) {
+            return false;
+        }
+
+        const num = +octet;
+        if (num < 0 || num > 255) {
+            return false;
         }
     }
-    
 
-    return count == 4? true: false
+    return true;
 }
