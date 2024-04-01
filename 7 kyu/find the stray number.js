2db2ad552b059e712e52cfc0831bab5f5f813393
Daria <113102018+dariiakurilenko@users.noreply.github.com>
2024-04-01 21:25:17 +0300

    Create find the stray number.js

---
+++ 7 kyu/find the stray number.js
@@ -0,0 +1,9 @@
+//You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+//Complete the method which accepts such an array, and returns that single different number.
+
+//The input array will always be valid! (odd-length >= 3)
+
+function stray(numbers) {
+    return numbers.find(number => numbers.indexOf(number) === numbers.lastIndexOf(number));
+}
