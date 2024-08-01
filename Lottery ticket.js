1fb399b744ac2f98e8ed914347a76a07652ddaf4
Daria <113102018+dariiakurilenko@users.noreply.github.com>
2024-08-01 19:01:26 +0300

    Delete Lottery ticket.js

--- Lottery ticket.js
+++
@@ -1,9 +0,0 @@
-//Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
-
-function bingo(ticket, win){
-   if(ticket.filter(a => a[0].split('').some(b => b.charCodeAt(0) == a[1])).length >= win)
-   {
-     return "Winner!";
-   }
-   return "Loser!";
-}
